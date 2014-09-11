1ffa58f0e94ff8bbba321bbdb1b5a13db542bf40
eric <onlinux@free.fr>
2014-09-11 18:58:39 +0200

    New date format. require node module 'moment'

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var clientIp = process.env.MYIP || getIPAddress();
-var zibaseIp = process.env.IP_ZIBASE|| "192.168.0.100";
+var zibaseIp = process.env.IP_ZIBASE || "192.168.0.100";
 
 var moment = require('moment');
-var dateFormat = "MMM DD HH:mm:ss";
+var dateFormat = "MMM DD YYYY HH:mm:ss";
 
-var dgram = require("dgram");
+var dgram = require('dgram');
 var server = dgram.createSocket("udp4");
 var client = dgram.createSocket("udp4");
 
 var b = new Buffer(70);
 b.fill(0);
 b.write('ZSIG\0', 0/*offset*/);
-b.writeUInt16BE(13,4); //command HOST REGISTERING (13)
+b.writeUInt16BE(13, 4); //command HOST REGISTERING (13)
 b.writeUInt32BE(dot2num(clientIp), 50); //Ip address
 b.writeUInt32BE(0x42CC, 54); // port 17100 0x42CC
 
@@ -21,54 +21,57 @@ console.log(b);
 console.log(b.toString('hex', 0, b.length));
 
 server.on("error", function (err) {
-  console.log("server error:\n" + err.stack);
-  server.close();
+    console.log("server error:\n" + err.stack);
+    server.close();
 });
 
 server.on("message", function (msg, rinfo) {
-  var date = moment();
-  console.log( date.format(dateFormat) + " " + msg.slice(70) );
+    var date = moment();
+    console.log(date.format(dateFormat) + " " + msg.slice(70));
 });
 
 server.on("listening", function () {
-  var address = server.address();
-  console.log("server listening " +
-      address.address + ":" + address.port);
+    var address = server.address();
+    console.log("server listening " +
+        address.address + ":" + address.port);
 });
 
-client.send(b, 0, b.length, 49999, zibaseIp, function(err, bytes) {
-  client.close();
+client.send(b, 0, b.length, 49999, zibaseIp, function (err, bytes) {
+    client.close();
 });
 
 server.bind(0x42CC, clientIp);
 
 
-process.on('SIGINT', function() {
+process.on('SIGINT', function () {
     console.log("Caught interrupt signal");
 
     var client = dgram.createSocket("udp4");
-    b.writeUInt16BE(22,4); //command HOST UNREGISTERING (22)
+    b.writeUInt16BE(22, 4); //command HOST UNREGISTERING (22)
     console.log(b);
-    client.send(b, 0, b.length, 49999, zibaseIp, function(err, bytes) {
-      console.log("Unregistering..." , bytes);
-      setTimeout( function(){
-              console.log("exit");
-              client.close();
-              process.exit()
-          }, 3000);
-    });    
+    client.send(b, 0, b.length, 49999, zibaseIp, function (err, bytes) {
+        console.log("Unregistering...", bytes);
+        setTimeout(function () {
+            console.log("exit");
+            client.close();
+            process.exit()
+        }, 3000);
+    });
 });
 
 function dot2num(dot) {
-var d = dot.split('.');
-return ((((((+d[0])*256)+(+d[1]))*256)+(+d[2]))*256)+(+d[3]);}
+    var d = dot.split('.');
+    return ((((((+d[0]) * 256) + (+d[1])) * 256) + (+d[2])) * 256) + (+d[3]);
+}
 
 function num2dot(num) {
-var d = num%256;
-for (var i = 3; i > 0; i--) { 
-num = Math.floor(num/256);
-d = num%256 + '.' + d;}
-return d;}
+    var d = num % 256;
+    for (var i = 3; i > 0; i--) {
+        num = Math.floor(num / 256);
+        d = num % 256 + '.' + d;
+    }
+    return d;
+}
 
 function getIPAddress() {
     var interfaces = require('os').networkInterfaces();
