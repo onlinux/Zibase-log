e7fafaadda0b84d3220ae4cfe7c8041b5d080d82
eric <onlinux@free.fr>
2014-09-12 14:58:52 +0200

    New branch Dev Retrieve name of probes, actuators, scenarios, sensors from zibase.net ZAPI.php

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var config = {};
 
-//IP de la zibase
-config.zibaseIp = process.env.IP_ZIBASE || '192.168.0.100';
 
-config.zibase = 'ZiBASEXXXXXX';
-config.token = 'xxxxxxxxxx';
+config.zibaseIp = process.env.IP_ZIBASE || '192.168.0.100'; // <- LAN IP address
+
+config.zibase = 'ZiBASEXXXXXX'; // <- Enter Min Identifier
+config.token = 'xxxxxxxxxx'; // <- Enter Token
 
 module.exports = config;
